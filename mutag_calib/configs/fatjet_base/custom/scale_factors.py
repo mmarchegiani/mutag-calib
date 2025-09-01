a39526471f7970417152df62c2e48528c80d1453
Matteo Marchegiani <matmarcheg@gmail.com>
2025-09-01 15:28:14 +0200

    Fix conflicts in weights

--- mutag_calib/configs/fatjet_base/custom/scale_factors.py
+++ mutag_calib/configs/fatjet_base/custom/scale_factors.py
@@ -41,3 +41,24 @@ def sf_trigger_prescale(events, year, params):
 
     return sf
 
+def sf_ptetatau21_reweighting(events, year, params):
+    '''Correction of jets observable by a 3D reweighting based on (pT, eta, tau21).
+    The function returns the nominal, up and down weights, where the up/down variations are computed considering the statistical uncertainty on data and MC.'''
+
+
+    cset = correctionlib.CorrectionSet.from_file(ptetatau21_reweighting[year])
+    corr = cset[f"FatJetGoodNMuon1_pt_eta_tau21_corr_{year}"]
+
+    cat = "pt300msd40"
+    nfatjet  = ak.num(events.FatJetGood.pt)
+    pos = ak.flatten(ak.local_index(events.FatJetGood.pt))
+    pt = ak.flatten(events.FatJetGood.pt)
+    eta = ak.flatten(events.FatJetGood.eta)
+    tau21 = ak.flatten(events.FatJetGood.tau21)
+
+    weight = {}
+    for var in ["nominal", "statUp", "statDown"]:
+        w = corr.evaluate(cat, var, pos, pt, eta, tau21)
+        weight[var] = ak.unflatten(w, nfatjet)
+
+    return weight["nominal"], weight["statUp"], weight["statDown"]
